be946ac6fd3e281eb159f32894a1470d4d6995cd
sarcosta33 <sarcosta@hartford.edu>
2023-11-01 08:28:52 -0400

    User input

--- RunMatrix.java
+++ RunMatrix.java
@@ -2,5 +2,15 @@ import java.util.Scanner;
 
 public class RunMatrix {
   public static void main(String[] args) {
+
+    int size;
+
+    Scanner scan = new Scanner(System.in);
+    System.out.print("Please enter the size of you matrix: ");
+    size = scan.nextInt();
+
+    System.out.println();
+
+    System.out.println("You matrix is " + size + " x " + size);
   }
 }
